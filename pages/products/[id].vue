e7da8a6bd61ae390ed5b53460ce3279c8fa2f4e2
IdaDanuartha <danuart21@gmail.com>
2023-08-30 15:36:55 +0800

    learn basic nuxt (add pages)

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Product Id : {{ id }}</h1>
+    </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
