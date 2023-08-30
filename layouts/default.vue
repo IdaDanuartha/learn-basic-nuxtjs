5399c065c910922fc91ef037d32277bfed3fe6bf
IdaDanuartha <danuart21@gmail.com>
2023-08-30 16:20:44 +0800

    add package (tailwindcss)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,21 @@
 <template>
-    <div>
-      <NuxtLink class="link" to="/">Home</NuxtLink>
-      <NuxtLink class="link" to="/about">About</NuxtLink>
-      <NuxtLink class="link" to="/products">Products</NuxtLink>
+    <div class="container mx-auto my-10">
+      <div class="flex justify-between bg-gray-800 text-gray-100 p-5 rounded-lg">
+        <div>
+            <h5 class="font-bold uppercase">Nuxt App</h5>
+        </div>
+        <div>
+            <NuxtLink class="link" to="/">Home</NuxtLink>
+            <NuxtLink class="link" to="/about">About</NuxtLink>
+            <NuxtLink class="link" to="/products">Products</NuxtLink>
+        </div>
+      </div>
   
-      <div>
+      <div class="mt-5">
         <slot />
       </div>
     </div>
-  </template>
+</template>
   
   <style scoped>
   .link {
--- layouts/products.vue
+++ layouts/products.vue
@@ -1,18 +1,24 @@
 <template>
-    <div>
-        <h1>Products Page</h1>
-        <ul>
-            <li>
+    <div class="container mx-auto my-10">
+      <div class="flex justify-center bg-gray-800 text-gray-100 p-5 rounded-lg">
+        <div>
+            <h5 class="font-bold uppercase">Products Page</h5>
+        </div>
+      </div>
+  
+      <div class="mt-5">
+        <ul class="flex justify-center">
+            <li class="text-xl mx-5 text-green-500 hover:text-green-300 duration-200">
                 <NuxtLink class="link" to="/products/1">Product 1</NuxtLink>
             </li>
-            <li>
+            <li class="text-xl mx-5 text-green-500 hover:text-green-300 duration-200">
                 <NuxtLink class="link" to="/products/2">Product 2</NuxtLink>
             </li>
-            <li>
+            <li class="text-xl mx-5 text-green-500 hover:text-green-300 duration-200">
                 <NuxtLink class="link" to="/products/3">Product 3</NuxtLink>
             </li>
         </ul>
-        <slot />
+      </div>
     </div>
 </template>
 
