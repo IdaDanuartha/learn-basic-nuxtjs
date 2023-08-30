e13c90645ee69093dca815c6f018a29c8099356c
IdaDanuartha <danuart21@gmail.com>
2023-08-30 15:44:16 +0800

    nuxt link

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <NuxtLink class="link" to="/">Home</NuxtLink>
+    <NuxtLink class="link" to="/about">About</NuxtLink>
+    <NuxtLink class="link" to="/products">Products</NuxtLink>
+
     <NuxtPage />
   </div>
 </template>
+
+<style scoped>
+.link {
+  margin-right: 10px;
+}
+</style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,6 +1,17 @@
 <template>
     <div>
         <h1>Products Page</h1>
+        <ul>
+            <li>
+                <NuxtLink class="link" to="/products/1">Product 1</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink class="link" to="/products/2">Product 2</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink class="link" to="/products/3">Product 3</NuxtLink>
+            </li>
+        </ul>
     </div>
 </template>
 
