08b550163bcc890c131856cc54446fb7646b513c
IdaDanuartha <danuart21@gmail.com>
2023-08-30 18:34:33 +0800

    reusable components

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,11 +1,21 @@
 <template>
-    <div>
-        <h1>Product Id : {{ id }}</h1>
+    <div class="grid grid-cols-2 gap-5">
+        <div class="mr-5">
+            <img :src="product.image" class="w-full" alt="">
+        </div>
+        <div>
+            <h3 class="text-4xl font-bold mb-10" v-html="product.title"></h3>
+            <h6 class="bg-gray-500 text-white inline-block px-5 py-2 capitalize mb-3 rounded-lg" v-html="product.category"></h6>
+            <p class="text-xl" v-html="product.description"></p>
+            <hr class="my-5">
+            <h6 class="text-lg" v-html="`$${product.price}`"></h6>
+        </div>
     </div>
 </template>
 
 <script setup>
 const { id } = useRoute().params
+const { data: product } = await useFetch(`https://fakestoreapi.com/products/${id}`)
 
 </script>
 
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,24 +1,6 @@
 <template>
     <div class="grid grid-cols-4 gap-10">
-        
-        <div v-for="p in products" :key="p.id" class="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-                <img class="rounded-t-lg" :src="p.image" alt="" />
-            </a>
-            <div class="p-5">
-                <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white" v-html="p.title"></h5>
-                </a>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400" v-html="p.description.substring(0,100) + '...'"></p>
-                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Add to Cart
-                    <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                    </svg>
-                </a>
-            </div>
-        </div>
-
+        <ProductCard :products="products" />
     </div>
 </template>
 
